14afaa8664f8de57dd7787fa950eb332c2ceb400
Melissa <melissa_hartung@teilnehmer.cdemy.de>
2025-10-02 15:10:23 +0200

    üben

--- bin/übung.dart
+++ bin/übung.dart
@@ -23,7 +23,7 @@ void main() {
   for (int i = 0; i < 10; i++) {
     for (int j = 0; j < 10; j++) {
       int produkt = i * j;
-      print('$i X $j =$produkt');
+      // print('$i X $j =$produkt');
     }
   }
 
@@ -35,5 +35,34 @@ void main() {
     return ergebnis;
   }
 
-  print(fakultaet(6));
+  // print(fakultaet(6));
+
+  for (int i = 0; i < 20; i++) {
+    for (int j = 0; j < 20; j++) {
+      int produkt = i * j;
+      // print('$i x $j = $produkt');
+    }
+  }
+
+  int summe2 = 0;
+  for (int i = 0; i < 100; i++) {
+    summe2 += i;
+  }
+  // print(summe2);
+
+  int summe3 = 0;
+  for (int i = 0; i < 900; i++) {
+    summe3 += i;
+  }
+  // print(summe3);
+
+  int fakultaet2(int wert) {
+    int ergebnis = 1;
+    for (int i = 1; i <= wert; i++) {
+      ergebnis = ergebnis * i;
+    }
+    return ergebnis;
+  }
+
+  print(fakultaet2(7));
 }
